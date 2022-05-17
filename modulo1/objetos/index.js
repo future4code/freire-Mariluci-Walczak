5f415962762a6f4f12553b458cd4c17c09255e25
Mariluci de Lima Walczak <mariluciwalcza@gmail.com>
2022-05-16 23:09:04 -0300

    Exercicios de funcoes dia 16 05 2022

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,86 @@
+//Exercícios de interpretação de código
+// Exercício nr.01 - a)
+//Matheus Nachtergaele 
+//Virginia Cavendish
+//{canal:"Globo",horario:"14h" 
+
+// Exercício nr.02
+//{nome:"Juca", idade:3, raca"SRD"}
+//{nome: 'Juba', idade: 3, raca: 'SRD'}
+//{nome: 'Jubo', idade: 3, raca: 'SRD'}
+
+// Exercício nr.03 - a)
+//false (no objeto pessoa, buscar o valor da chave backender)
+//undefined (falta valor, chave não foi declarada no objeto pessoa)
+
+
+//Exercícios de escrita de código
+
+// Exercício nr.01 - a)
+ /* const pessoa = {
+    nome:"Amanda",
+    apelidos:["Amandinha","Mandinha","Mandi"]
+}
+
+function recebeObjeto(pessoa){
+        
+    return recebeObjeto
+
+} 
+console.log(`"Eu sou ${pessoa.nome}, mas pode me chamar de: ${pessoa.apelidos}"`)
+
+// Exercício nr.01 - b)
+
+ const novoObjeto = {
+    ...pessoa,
+    listaApelidos:["Dinha","Dindinha","Mandinha"]
+}
+console.log(novoObjeto.listaApelidos)
+
+recebeObjeto(novoObjeto) */
+
+// Exercício nr.02  
+
+console.log(100)
+
+ const pessoaUm = {
+    nome:"Bruno",
+    idade:23,
+    profissao:"Instrutor"
+}
+
+const pessoaDois = {
+    nome:"Karoline",
+    idade:26,
+    profissao:"Desenvolvedora"
+}
+
+function minhaFuncaoUm(pessoaUm){
+    const listaPessoaUm = []
+    listaPessoaUm.nome
+    listaPessoaUm.nome.length
+    listaPessoaUm.idade
+    listaPessoaUm.profissao
+    listaPessoaUm.profissao.length 
+    return listaPessoaUm
+}
+    
+console.log(pessoaUm) 
+
+
+
+
+
+
+
+
+
+
+// Exercício nr.03
+
+
+
+
+
+
+
