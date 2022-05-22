cd539ec501efb4c46582e38d61c8f2b98c6dd51f
Mariluci de Lima Walczak <mariluciwalcza@gmail.com>
2022-05-22 19:37:52 -0300

    Projeto de Fixacao BlackJack 22052022

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -13,54 +13,49 @@
 
 alert("Boas vindas ao jogo de BlackJack !")
 
-const mensagem = ""
-const retorna = confirm("Quer iniciar uma nova rodada ?")
+let retorna =  confirm("Quer iniciar uma nova rodada ?")
+// A rodada inicia sortear duas cartas ( o método já está pronto, é só chamar)
 
-if(confirm === "OK") {
-	retorna = true
-} else {
-	alert ("O jogo acabou")
-}  
+if(retorna) {
+      let primeiraCartaUsuario = comprarCarta()
+      let segundaCartaUsuario = comprarCarta()
+      let primeiraCartaComputador = comprarCarta()
+      let segundaCartaComputador = comprarCarta()
 
-carta = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-arrayCartaUsuario = [texto,valor]
-arrayCartaComputador = [texto,valor]
-const somaCartasUsuario = 0
-const somaCartasComputador = 0
+//Guardar o valor e a carta de cada jogador, somando em uma variável para cada jogador
+   
+      let pontosUsuario = primeiraCartaUsuario.valor + segundaCartaUsuario.valor
+      let pontosComputador = primeiraCartaComputador.valor + segundaCartaComputador.valor
 
-if(retorna === true){
-   if (somaCartasUsuario <=21){
-      cartasUsuario[iu] = comprarCarta()
-      arrayCartaUsuario[texto,valor] = carta.valor,carta.texto
-      somaCartasUsuario = somaCartasUsuario + cartasUsuario[iu].texto
-      iu++
-   } else if(somaCartasComputador <=21){
-      cartasComputador[ic] =comprarCarta()
-      arrayCartaComputador[texto,valor] = carta.valor,carta.texto
-      somaCartasComputador = somaCartasComputador + cartasComputador[ic].texto
-      ic++
+// Mostrar no console a carta e pontuação de cada jogador
+   
+      console.log (`Cartas do Usuário : ${primeiraCartaUsuario.texto} - ${segundaCartaUsuario.texto} - pontos = ${pontosUsuario}`)
+   
+      console.log (`Cartas do Computador : ${primeiraCartaComputador.texto} - ${segundaCartaComputador.texto} - pontos = ${pontosComputador}`)
+   
+// Mostrar no console quem foi o vencedor do jogo
+      if(pontosUsuario < pontosComputador){
+         console.log("O computador ganhou!")
+      } else if(pontosUsuario > pontosComputador){
+         console.log("O usuário ganhou !")
+      } else if(pontosUsuario === pontosComputador){
+         console.log("Empate!")
+      }
+   
+   } else {
+      console.log("O jogo acabou")
    }
-   fimDeJogo()
-}
 
-const carta = comprarCarta(); 
-// Sorteia uma carta. Por exemplo, o rei de ouros
 
-console.log(carta.texto) 
-// imprime o texto da carta. Nesse caso: "K♦️"
 
-console.log(carta.valor) 
-// imprime o valor da carta (um número). Nesse caso: 10)
 
-const totalizar = fimDeJogo ()
-   if (somaCartasUsuario < somaCartasComputador){
-      console.log (`Computador ganhou !`)
-      console.log (`${arrayCartaComputador[valor.texto]}`)
-   } else if(somaCartasUsuario > somaCartasComputador){
-      console.log (`Usuário ganhou !`)
-      console.log (`${arrayCartaUsuario[valor.texto]}`)
-   }else{
-      console.log (`Empate !`)
-      console.log (`${arrayCartaUsuario[valor.texto]}`)
-      console.log (`${arrayCartaComputador[valor.texto]}`)
-   }
+
+
+
+
+
+
+
+
+
+
