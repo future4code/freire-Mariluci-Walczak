4e28c90b7acf74c56b192e83adc99da03b310efb
Mariluci de Lima Walczak <mariluciwalcza@gmail.com>
2022-06-23 21:48:31 -0300

    primeira atualização

---
+++ modulo2/projeto-labefy/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  state ={
+    telaAtual : "cadastro"
+  }
+  render () {
+    return (
+      <div>
+        Continua após essa atualização
+      </div>
+    )
+  }
+}
+
