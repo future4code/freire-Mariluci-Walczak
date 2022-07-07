4a49b60f702589d141e479b0306bc6d59b18533a
Mariluci de Lima Walczak <mariluciwalcza@gmail.com>
2022-07-07 17:22:02 -0300

    Atualizando -  não finalizei, ainda vou terminar

---
+++ modulo3/astromatch/src/components/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+
+export default function Card(){
+    const [perfil, setPerfil] = useState({});
+    const [selecionarPerfil, setSelecionarPerfil] = useState();
+
+    
+}
