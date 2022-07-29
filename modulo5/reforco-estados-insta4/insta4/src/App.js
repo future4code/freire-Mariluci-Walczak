4176b62bd77728f3652fba26264ce67084620337
Mariluci de Lima Walczak <mariluciwalcza@gmail.com>
2022-07-28 22:02:07 -0300

    Exercício do dia 28 - Reforço - insta4

--- modulo5/reforco-estados-insta4/insta4/src/App.js
+++ modulo5/reforco-estados-insta4/insta4/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Post from "./components/Post/Post";
 
@@ -9,25 +9,94 @@ const MainContainer = styled.div`
   align-items: center;
 `;
 
+const FormContainer = styled.div`
+        display:grid;
+        justify-content: space-between;
+        flex-direction: row;
+        /* height: 180px; */
+        /* width: 240px; */
+        /* border: 1px solid gray; */
+        /* margin: 15px; */
+        padding: 20px;
+      `;
+
 function App() {
+
+  const [posts, setPosts] = useState([
+    {
+      nomeUsuario: "Karol",
+      fotoUsuario: "https://picsum.photos/51/51",
+      fotoPost: "https://picsum.photos/200/155"
+    },
+    {
+      nomeUsuario: "Fábio",
+      fotoUsuario: "https://picsum.photos/52/50",
+      fotoPost: "https://picsum.photos/200/150"
+    },
+    {
+      nomeUsuario: "Jun",
+      fotoUsuario: "https://picsum.photos/50/53",
+      fotoPost: "https://picsum.photos/200/160"
+    }
+  ]);
+
+  const [valorInputNomeUsuario, setValorInputNomeUsuario] = useState("");
+  const [valorInputFotoUsuario, setValorInputFotoUsuario] = useState("");
+  const [valorInputFotoPost, setValorInputFotoPost] = useState("");
+
+  const adicionaPosts = () => {
+    const novoPosts = {
+      nomeUsuario: valorInputNomeUsuario,
+      fotoUsuario: valorInputFotoUsuario,
+      fotoPost: valorInputFotoPost
+    };
+
+    const novoArrayPosts = [...posts, novoPosts];
+    setPosts(novoArrayPosts);
+
+  };
+
+  const onChangeInputNomeUsuario = (event) => {
+    setValorInputNomeUsuario(event.target.value);
+  };
+  const onChangeInputFotoUsuario = (event) => {
+    setValorInputFotoUsuario(event.target.value)
+  };
+  const onChangeInputFotoPost = (event) => {
+    setValorInputFotoPost(event.target.value)
+  };
+
+  const listaDeComponentesPost = posts.map((post, index) => {
+    return (
+      <Post key={index}
+        nomeUsuario={post.nomeUsuario}
+        fotoUsuario={post.fotoUsuario}
+        fotoPost={post.fotoPost}>
+      </Post>
+    )
+  })
+
   return (
     <MainContainer>
-      <Post
-        nomeUsuario={"Paulinha"}
-        fotoUsuario={"https://picsum.photos/51/51"}
-        fotoPost={"https://picsum.photos/200/155"}
-      />
-      
-      <Post
-        nomeUsuario={"Julinha"}
-        fotoUsuario={"https://picsum.photos/52/50"}
-        fotoPost={"https://picsum.photos/200/150"}
-      />
-      <Post
-        nomeUsuario={"Luizinha"}
-        fotoUsuario={"https://picsum.photos/50/53"}
-        fotoPost={"https://picsum.photos/200/160"}
-      />
+      <FormContainer>
+          <input
+            placeholder="Nome Usuário"
+            value={valorInputNomeUsuario}
+            onChange={onChangeInputNomeUsuario}
+          />
+          <input
+            placeholder="Foto do Usuário"
+            value={valorInputFotoUsuario}
+            onChange={onChangeInputFotoUsuario}
+          />
+          <input
+            placeholder="Foto do Post"
+            value={valorInputFotoPost}
+            onChange={onChangeInputFotoPost}
+          />
+          <button onClick={adicionaPosts}>Postar</button>
+      </FormContainer>
+      {listaDeComponentesPost}
     </MainContainer>
   );
 }
