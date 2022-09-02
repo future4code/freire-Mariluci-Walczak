203d7397685f98d983af7a6d1ea1fa3d563d2dfe
Mariluci de Lima Walczak <mariluciwalcza@gmail.com>
2022-09-01 21:40:19 -0300

    Projeto LabECommerce

---
+++ modulo6/labecommerce/projeto-LabECommerce/src/services/getFullAddress.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { Request, Response } from "express";
+import { Address } from "../types/typeAddress";
+
+/* export const getFullAddress = async (cep:string): Promise<Address | undefined> => {
+    try {
+
+        const result = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+       
+        const address: Address = {
+            cep: result.data.cep,
+            logradouro: result.data.logradouro,
+            complemento: result.data.complemento,
+            bairro: result.data.bairro,
+            cidade: result.data.localidade,
+            estado: result.data.uf
+        }
+        return address
+    } catch (error: any) {
+        return undefined;
+    }
+
+} */
