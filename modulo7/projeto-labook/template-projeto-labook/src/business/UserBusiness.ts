de9b3c6c5f66366cfafea9ffba973814d6bbfe00
Mariluci de Lima Walczak <mariluciwalcza@gmail.com>
2022-09-23 23:28:09 -0300

    PROJETO LABOOK 02

--- modulo7/projeto-labook/template-projeto-labook/src/business/UserBusiness.ts
+++ modulo7/projeto-labook/template-projeto-labook/src/business/UserBusiness.ts
@@ -1,4 +1,5 @@
 import { UserDatabase } from "../database/UserDatabase"
+import { ISignupInputDTO } from "../models/User"
 import { Authenticator } from "../services/Authenticator"
 import { HashManager } from "../services/HashManager"
 import { IdGenerator } from "../services/IdGenerator"
@@ -9,6 +10,33 @@ export class UserBusiness {
         private idGenerator: IdGenerator,
         private hashManager: HashManager,
         private authenticator: Authenticator
-    ) {}
+    ) { }
 
+    public signup = async (input: ISignupInputDTO) => {
+        const { name, email, password }  = input
+
+        if (!name || !email || !password) {
+            throw new Error("Uma ou mais informações faltando")
+        }
+
+        if (typeof name !== "string" || name.length < 3) {
+            throw new Error("Name inválido, deve ter no mínimo 3 caracteres")
+        }
+
+        if (typeof email !== "string" || email.length < 3) {
+            throw new Error("'email' inválido")
+        }
+
+        if (!email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+            throw new Error(" 'email' inválido")
+        }
+
+        if (typeof password !== "string" || password.length < 6) {
+            throw new Error(" 'password' inválido, deve ter no mínimo 6 caracteres")
+        }
+
+        const isEmailAlreadyExist = await this.userDatabase
+    }
 }
+
+
--- modulo7/projeto-labook/template-projeto-labook/src/index.ts
+++ modulo7/projeto-labook/template-projeto-labook/src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(cors())
 
 app.listen(process.env.PORT || 3003, () => {
-    console.log(`Servidor rodando na porta ${process.env.PORT || 3003}`)
+    console.log(`OK - Servidor rodando na porta ${process.env.PORT || 3003}`)
 })
 
 app.use("/ping", pingRouter)
